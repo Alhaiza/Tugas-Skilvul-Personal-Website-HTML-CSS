fb240dd9ec8e4eb66a92e17d5747278721b23b52
Syarif Muhammad Alhaiza <alqadrihaiza@gmail.com>
2023-10-06 22:02:21 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var toggle = document.querySelector(".toggle input");
+var nav = document.querySelector("nav ul");
+
+toggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
